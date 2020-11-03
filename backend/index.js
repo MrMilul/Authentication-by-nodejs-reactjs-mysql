3e5e6b584951f8c8fbd07f127ccea2b07fb97e51
MrMilul <miladmizani@yahoo.com>
2020-11-03 11:14:32 +0200

    hashing password by bcrypt

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+//hasshing password
+const bcrypt = require('bcrypt');
+const saltRound = 10; 
 
 app.use(cors()); 
 app.use(express.json());
@@ -23,15 +26,26 @@ app.post('/register', (req, res)=>{
     const f_name = req.body.f_name;
     const l_name = req.body.l_name;
     const email = req.body.email;
-        const password = req.body.password
-
+    const password = req.body.password
     const insertQuery = "INSERT INTO register (username, f_name, l_name, Email, password) VALUES (?, ?, ?, ?, ?)"
+
+    //sending hashed password to the database.
+    bcrypt.hash(password, saltRound, (err, hash)=>{
+        if(err){
+            console.log(err)
+        }
+
+        db.query(insertQuery, [username, f_name, l_name, email, hash], (err, result)=>{
+            if (err){
+                console.log(err)
+            }
+        })
+
+    })
+
+    
    
-   db.query(insertQuery, [username, f_name, l_name, email, password], (err, result)=>{
-       if (err){
-           console.log(err)
-       }
-   })
+   
 
 })
 
@@ -39,17 +53,26 @@ app.post('/login', (req, res)=>{
     const username = req.body.username;
     const password = req.body.password;
 
-    const selectQuery = "SELECT * FROM register WHERE username=? AND password=?"
+    const selectQuery = "SELECT * FROM register WHERE username=?"
 
-    db.query(selectQuery, [username,password], (err, result)=>{
+    db.query(selectQuery, username, (err, result)=>{
         if(err){
             res.send({err:err})
         }
-
+        //check entered password by hashing one
         if(result.length > 0){
-            res.send(result)
+            bcrypt.compare(password, result[0].password, (error, response)=>{
+                if(error){
+                    console.log(error)
+                }
+                if(response){
+                    res.send(result)
+                }else{
+                    res.send({message:"Wrong Username/Password combination"})
+                }
+            })
         }else{
-            res.send({message:"Wrong Username/Password combination"})
+            res.send({message:"Incorrect Username"})
         }
     })
 })
